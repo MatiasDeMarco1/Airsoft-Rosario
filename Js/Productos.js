7b5918532924021cc445fec2b268c8a5f2ba8c9a
Matías De Marco <demarcomatias25@gmail.com>
2023-08-07 12:36:35 -0300

    bugs2

--- Js/Productos.js
+++ Js/Productos.js
@@ -31,7 +31,7 @@ contadorCarritoMobile()
 function spiner1(){
     let spinner = document.getElementById('spinner')
     spinner.style.display = 'flex'
-    fetch("../json/productos.json")
+    fetch("../productos.json")
     .then((response) => response.json())
     .then(productoCarga => {
         setTimeout(()=>{
