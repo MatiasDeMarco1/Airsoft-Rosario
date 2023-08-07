c82a97c30283a4b2f7ac278986e9aab2c49b8528
Matías De Marco <demarcomatias25@gmail.com>
2023-08-07 12:42:41 -0300

    bugs4

--- Js/Index.js
+++ Js/Index.js
@@ -30,7 +30,7 @@ contadorCarritoMobile()
 cargaIndex()
 cargaIndexCelu()
 function cargaIndex() {
-    fetch("../productosDestacadosJSON.json")
+    fetch("productosDestacadosJSON.json")
     .then((response) => response.json())
     .then(destacados => {
         for (const producto of destacados){
